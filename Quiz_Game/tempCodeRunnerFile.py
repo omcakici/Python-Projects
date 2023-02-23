e053b762f97555560608b917e723245ce70d9485
omcakici <o.f.cakici@student.vu.nl>
2023-02-23 08:25:54 +0100

    Hist Painting Project

--- Quiz_Game/tempCodeRunnerFile.py
+++ Quiz_Game/tempCodeRunnerFile.py
@@ -1,12 +1,1 @@
-for q in question_data["results"]:
-    text = q["question"]
-    answer = q["correct_answer"]
-    question = Question(text, answer)
-    question_bank.append(question)
-
-random.shuffle(question_bank)
-quiz_logic = QuizBrain(question_bank)
-
-## while quiz bank has still quesiton keep asking
-while quiz_logic.still_has_question():
-    quiz_logic.next_question()
+print('--------------------------------')
